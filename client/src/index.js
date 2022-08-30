eabe17f96611c8709080291ac171450700401bf6
nisabmohd <nisabmohd@gmail.com>
2022-08-30 23:02:06 +0530

    service worker PWA added

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +11,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+serviceWorkerRegistration.register();
