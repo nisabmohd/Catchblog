9f1cfe2434503467b6d36af10adbbefe82a74a0a
Mohd Nisab Alam <76525761+nisabmohd@users.noreply.github.com>
2022-09-02 13:33:44 +0530

    Update SearchUser.js

--- client/src/Pages/SearchUser.js
+++ client/src/Pages/SearchUser.js
@@ -50,14 +50,15 @@ export const SearchUser = () => {
                 {
                     loading && <Box style={{ width: '100%', display: 'flex', alignContent: 'center', marginTop: '10px' }}><CircularProgress size='10' thickness={4} style={{ margin: 'auto' }} color="inherit" /></Box>
                 }
-                {
-                    loading === false && users.length === 0 ? <Box style={{ width: '100%', display: 'flex', alignContent: 'center', marginTop: '10px' }}><h4 style={{ margin: 'auto' }} >Nothing to see here</h4></Box> : <></>
-                }
-
+                
                 { <>
                     <button onClick={() => navigate(`/searchuser?q=${searchParams.get("q")}`)} className='newpostbtn' style={{ fontFamily: 'Poppins', minWidth: 'fit-content', width: '125px', color: 'white', border: 'none', outline: 'none', background: 'rgb(66 66 66)', height: '33px', borderRadius: '5px', cursor: 'pointer', marginRight: '15px' }} variant="outlined">Serach User</button>
                     <button onClick={() => navigate(`/search?q=${searchParams.get("q")}`)} className='newpostbtn' style={{ fontFamily: 'Poppins', minWidth: 'fit-content', width: '125px', color: 'white', border: 'none', outline: 'none', background: 'transparent', height: '33px', borderRadius: '5px', cursor: 'pointer', marginRight: '15px', marginBottom: '19px' }} variant="outlined">Serach Post</button>
                 </>}
+{
+                    loading === false && users.length === 0 ? <Box style={{ width: '100%', display: 'flex', alignContent: 'center', marginTop: '10px' }}><h4 style={{ margin: 'auto' }} >Nothing to see here</h4></Box> : <></>
+                }
+
 
                 {users.length !== 0 && <h3 style={{ marginBottom: '33px' }}>Showing Users Results for ' {searchParams.get("q")} '</h3>}
                 {users.length !== 0 && <div style={{ marginBottom: '45px' }}>
