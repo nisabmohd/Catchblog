accb9a02c64fcad0a3ad7b928eac6be80f61d841
nisabmohd <nisabmohd@gmail.com>
2022-09-06 01:34:03 +0530

    synatx edit error handled and useEffect added in synatx highlighter

--- client/src/components/Syntax.js
+++ client/src/components/Syntax.js
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { darcula,oneLight} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { darcula, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { AppContext } from "../App";
 import { useContext } from 'react';
 
 export const Syntax = (props) => {
     const context = useContext(AppContext)
-    const [state] = useState(props.children[0])
-    return (
-        <div style={{borderRadius:'5px',overflow:'hidden',fontSize:'14px',fontFamily:'Fira Code'}}>
-            <SyntaxHighlighter language='java' style={context.dark?darcula:oneLight}>
-                {state}
-            </SyntaxHighlighter>
+    const [state, setState] = useState("")
+    useEffect(() => {
+        setState(props.children[0])
+    }, [props.children])
+    try {
+        return (
+            <div style={{ borderRadius: '5px', overflow: 'hidden', fontSize: '14px', fontFamily: 'Fira Code' }}>
+                <SyntaxHighlighter language='java' style={context.dark ? darcula : oneLight}>
+                    {JSON.parse(JSON.stringify(state))}
+                </SyntaxHighlighter>
+            </div>
+        )
+    } catch (err) {
+        <div style={{ borderRadius: '5px', overflow: 'hidden', fontSize: '14px', fontFamily: 'Fira Code' }}>
+            <p>some error in code</p>
         </div>
-    )
+    }
+
 }
