4357f70641c40bca928f51a28dab7f222879f067
nisabmohd <nisabmohd@gmail.com>
2022-09-10 23:37:58 +0530

    notification loading added

--- client/src/Pages/Notifications.js
+++ client/src/Pages/Notifications.js
@@ -1,3 +1,4 @@
+import { CircularProgress } from '@mui/material'
 import { Box } from '@mui/system'
 import axios from 'axios'
 import React, { useContext, useEffect } from 'react'
@@ -9,29 +10,35 @@ import { UserCard } from '../components/UserCard'
 
 export const Notifications = () => {
   const context = useContext(AppContext)
-  const [not,setNot]=useState([])
+  const [not, setNot] = useState([])
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
-    async function fetch(){
+    async function fetch() {
       const resp = await axios.get(`${url}/user/notifications/${context.auth.uid}`)
-      const resp1=await axios.put(`${url}/user/readnotification/${context.auth.uid}`)
-      if(resp1.data.message) context.setHaveNotification(false)
+      const resp1 = await axios.put(`${url}/user/readnotification/${context.auth.uid}`)
+      setLoading(false)
+      if (resp1.data.message) context.setHaveNotification(false)
       setNot(resp.data)
     }
     fetch();
   }, [context])
-  
+
 
   return (
     <div className='container'>
       <div className="container-left">
         <h3 style={{ marginBottom: '33px' }}>Notifications</h3>
         {
-          not.map(item=>{
-            return <Notify key={item.id} type={item.type} postid={item.postid} date={item.date} uid={item.uid}/>
-          })
+          loading && <Box style={{ width: '100%', height: '80vh', display: 'flex', alignContent: 'center', marginTop: '10px' }}><CircularProgress style={{ margin: 'auto' }} color="inherit" /></Box>
         }
         {
-          not.length===0 && <Box style={{ width: '100%', display: 'flex', alignContent: 'center', marginTop: '10px' }}><h4 style={{ margin: 'auto' }} >Nothing to see here</h4></Box>
+          loading === false && not.length === 0 ? <Box style={{ width: '100%', height: '80vh', display: 'flex', alignContent: 'center', marginTop: '10px' }}><h4 style={{ margin: 'auto' }} >Nothing to see here</h4></Box> : <></>
+        }
+        {
+          not.map(item => {
+            return <Notify key={item.id} type={item.type} postid={item.postid} date={item.date} uid={item.uid} />
+          })
         }
       </div>
       <div className="container-right notification-page-resp">
